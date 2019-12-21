19dcf210ef1ccd4b7e1927ffa753282db9636d16
Charles Wang <50273755+thewangclass@users.noreply.github.com>
2019-12-21 17:14:37 -0600

    Prototype1 relative force to control car

--- create-with-code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ create-with-code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private Variables
-    [SerializeField] private float speed = 5.0f;
+    //[SerializeField] private float speed = 5.0f;
+    [SerializeField] private float horsePower = 0;
     [SerializeField] private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float verticalInput;
+    private Rigidbody playerRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,7 +25,9 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // Move the vehicle forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+
         // Turn vehicle right/left based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
